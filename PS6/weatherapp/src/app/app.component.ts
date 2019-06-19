96eb93670c431cc2f8cacd8c67f8015be83ee433
jingyu98 <jingyu98@bu.edu>
2019-06-18 23:40:33 -0400

    PS6 finished

---
+++ PS6/weatherapp/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Weather } from './model/weather';
+import { WeatherService } from './service/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'weatherapp';
+  selectedWeather: Weather = null;
+  weathers: Weather;
+
+  getWeathers(): void{
+    this.weatherService.getWeathers()
+      .subscribe(weathers =>{
+        this.weathers = weathers;
+        console.log(`current weather fetched ${JSON.stringify(this.weathers)}`)
+      });
+  }
+
+  displayWeatherDetail(weather: Weather){
+    this.selectedWeather = weather;
+  }
+
+
+  constructor(private weatherService: WeatherService){
+
+  }
+
+  ngOnInit(){
+    this.getWeathers();
+  }
+}
